538555125f8841d9acc364e2f753dc6f4787cc3a
rirubelhossain <rubelphp94@gmail.com>
2022-06-09 00:28:06 +0600

    Pagination

--- index.php
+++ index.php
@@ -14,9 +14,19 @@ $fm = new Format();
 
 	<div class="contentsection contemplete clear">
 		<div class="maincontent clear">
-
+		<!--Pagination-->
 		<?php 
-		$query = "SELECT * FROM tbl_post limit 3";
+			$per_page = 3 ;
+			if( isset($_GET['page'])){
+				$page = $_GET['page'];
+			}else{
+				$page = 1 ;
+			}
+			$start_from = ($page -1 )*$per_page ;
+			?>
+		<!--Pagination-->
+		<?php 
+		$query = "SELECT * FROM tbl_post limit $start_from , $per_page";
 		$post = $db->select($query);
 		if( $post ){ 
 
@@ -35,10 +45,27 @@ $fm = new Format();
 					<a href="post.php?id=<?php echo $row['id'];?>">Read More</a>
 				</div>
 			</div>
-		<?php } ?>
+		<?php } ?> <!--End while loop-->
+		<!--Pagination-->
+		<?php 
+		$query = "SELECT * from tbl_post";
+		$result = $db->select($query);
+		$total_rows = mysqli_num_rows($result);
+		$total_pages = ceil($total_rows / $per_page);
+		
+		echo "<span class='pagination'> <a href='index.php?page=1'>".'First Page'."</a>" ;
+		
+		
+		
+		for($i = 1 ; $i <= $total_pages ; ++$i){
+			echo "<a href='index.php?page=".$i." "."'>"."   ".$i."  "."</a>";
+		}
+		?>
+
+		<?php echo "<a href='index.php?page=$total_pages'>".'Last Page'."</a></span>"?>
+		<!--Pagination-->
 		<?php }else {
 			header("Location: 404.php");
-
 		}?>
 			
 		
