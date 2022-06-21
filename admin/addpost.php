13e0100cb209f5c5f22b809f4a807b4efbeee187
rirubelhossain <rubelphp94@gmail.com>
2022-06-22 01:23:55 +0600

    Add Posts From admin panel

--- admin/addpost.php
+++ admin/addpost.php
@@ -6,8 +6,62 @@
 		
             <div class="box round first grid">
                 <h2>Add New Post</h2>
+<?php
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    
+    $title = mysqli_real_escape_string($db->link , $_POST['title']);
+    $cat = mysqli_real_escape_string($db->link , $_POST['cat']);
+    //$image = mysqli_real_escape_string($db->link , $_POST['image']);
+    $body = mysqli_real_escape_string($db->link , $_POST['body']);
+    $tags = mysqli_real_escape_string($db->link , $_POST['tags']);
+    $author = mysqli_real_escape_string($db->link , $_POST['author']);
+
+    ///image validation code here 
+    $permited  = array('jpg', 'jpeg', 'png', 'gif');
+    $file_name = $_FILES['image']['name'];
+    $file_size = $_FILES['image']['size'];
+    $file_temp = $_FILES['image']['tmp_name'];
+
+    $div = explode('.', $file_name);
+    $file_ext = strtolower(end($div));
+    $unique_image = substr(md5(time()), 0, 10).'.'.$file_ext;
+    $uploaded_image = "upload/".$unique_image;
+
+    if( $title == "" || $cat == "" || $body == "" || $tags == "" || $author == ""){
+        echo "<span class='error'>Filed must not be empty!</span>";
+    }elseif(empty($file_name)) {
+        echo "<span class='error'>Please Select any Image !</span>";
+        
+       }elseif ($file_size >1048567) {
+        echo "<span class='error'>Image Size should be less then 1MB!
+        </span>";
+
+       } elseif (in_array($file_ext, $permited) === false) {
+        echo "<span class='error'>You can upload only:-"
+        .implode(', ', $permited)."</span>";
+
+       } else{
+            move_uploaded_file($file_temp, $uploaded_image);
+            $query = "INSERT INTO tbl_post(cat,title,body,image,author,tags) 
+            VALUES('$cat','$title','$body','$uploaded_image','$author','$tags')";
+            $inserted_rows = $db->insert($query);
+
+            if ($inserted_rows) {
+                echo "<span class='success'>Post Inserted Successfully.
+                </span>";
+            }else {
+                echo "<span class='error'>Post Not Inserted !</span>";
+            }
+
+
+    }
+}
+
+?>
+
                 <div class="block">               
-                 <form action="" method="" enctype="multipart/form-data">
+                 <form action="addpost.php" method="POST" enctype="multipart/form-data">
                     <table class="form">
                        
                         <tr>
@@ -15,7 +69,7 @@
                                 <label>Title</label>
                             </td>
                             <td>
-                                <input type="text" placeholder="Enter Post Title..." class="medium" />
+                                <input type="text" name ="title" placeholder="Enter Post Title..." class="medium" />
                             </td>
                         </tr>
                      
@@ -24,7 +78,7 @@
                                 <label>Category</label>
                             </td>
                             <td>
-                                <select id="select" name="select">
+                                <select id="select" name="cat">
                                     <option>Select Category</option>
 
                             <?php
@@ -35,7 +89,7 @@
                                     while($row = $catresult->fetch_assoc()){
 
                             ?>                        
-                                    <option value="1"><?php echo $row['name'];?></option>
+                                    <option value="<?php echo $row['id'];?>"><?php echo $row['name'];?></option>
                                     <?php }}?>
                                 </select>
                                 
@@ -46,7 +100,7 @@
                                 <label>Upload Image</label>
                             </td>
                             <td>
-                                <input type="file" />
+                                <input type="file" name="image" />
                             </td>
                         </tr>
                         <tr>
@@ -54,7 +108,23 @@
                                 <label>Content</label>
                             </td>
                             <td>
-                                <textarea class="tinymce"></textarea>
+                                <textarea class="tinymce" name="body"></textarea>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <label>Tags</label>
+                            </td>
+                            <td>
+                                <input type="text" name ="tags" placeholder="Enter Tags..." class="medium" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <label>Author</label>
+                            </td>
+                            <td>
+                                <input type="text" name ="author" placeholder="Enter Author Name..." class="medium" />
                             </td>
                         </tr>
 						<tr>
