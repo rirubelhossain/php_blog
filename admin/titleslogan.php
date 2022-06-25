dcd1e1dd76a59b9e3a6783ca766660286a9582b8
rirubelhossain <rubelphp94@gmail.com>
2022-06-25 07:08:33 +0600

    table for logo , title , slogan

--- admin/titleslogan.php
+++ admin/titleslogan.php
@@ -1,11 +1,40 @@
 ﻿<?php include 'inc/header.php';?>
 <?php include 'inc/sidebar.php'?>
+<style>
+.rightside{
+float:left ;
+width: 20%
+}
+.rightside img{
+height: 150px ;
+width: 170px ;
+}
+.leftside {
+float: left ;
+width:70% ;
+}
 
+</style>
         <div class="grid_10">
 		
             <div class="box round first grid">
                 <h2>Update Site Title and Description</h2>
-                <div class="block sloginblock">               
+
+
+<?php
+$query = "select * from title_slogan where id = '1'";
+$blog_title = $db->select($query);
+
+if( $blog_title ){
+    while( $result = $blog_title->fetch_assoc()){
+
+     
+ 
+
+?>
+                <div class="block sloginblock">  
+                    
+                <div class="leftside">
                  <form>
                     <table class="form">					
                         <tr>
@@ -13,7 +42,7 @@
                                 <label>Website Title</label>
                             </td>
                             <td>
-                                <input type="text" placeholder="Enter Website Title..."  name="title" class="medium" />
+                                <input type="text" value="<?php echo $result['title'] ;?>" name="title" class="medium" />
                             </td>
                         </tr>
 						 <tr>
@@ -21,10 +50,17 @@
                                 <label>Website Slogan</label>
                             </td>
                             <td>
-                                <input type="text" placeholder="Enter Website Slogan..." name="slogan" class="medium" />
+                                <input type="text" value="<?php echo $result['slogan'];?>" name="slogan" class="medium" />
                             </td>
                         </tr>
-						 
+                        <tr>
+                            <td>
+                                <label>Upload Logo</label>
+                            </td>
+                            <td>
+                                <input type="file" name="logo" />
+                            </td>
+                        </tr>
 						
 						 <tr>
                             <td>
@@ -36,6 +72,17 @@
                     </table>
                     </form>
                 </div>
+                <div class = "rightside">
+                    
+                    <img src = "<?php echo $result['logo'];?>" alt="logo"/>
+
+                </div>
+
+               
+
+
+                </div>
+                <?php }}?>
             </div>
         </div>
 <?php include 'inc/footer.php' ;?>
