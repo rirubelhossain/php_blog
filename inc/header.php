9b5a30d00f7188baf5d2f33fd34349f61e5099eb
rirubelhossain <rubelphp94@gmail.com>
2022-06-27 23:42:07 +0600

    footer update

--- inc/header.php
+++ inc/header.php
@@ -105,6 +105,7 @@ $(window).load(function() {
 <div class="navsection templete">
 	<ul>
 		<li><a id="active" href="index.php">Home</a></li>
+		
 		<li><a href="about.php">About</a></li>	
 		<li><a href="contact.php">Contact</a></li>
 	</ul>
