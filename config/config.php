5905537e46208ddf9e94982cbcf94525ccfe0188
rirubelhossain <rubelphp94@gmail.com>
2022-07-05 08:28:49 +0600

    Adding meta keywords

--- config/config.php
+++ config/config.php
@@ -4,4 +4,5 @@ define("DB_USER", "root");
 define("DB_PASS", "");
 define("DB_NAME", "db_blog");
 define("TITLE", "Live Project with Rubel");
+define("KEYWORDS" , "PHP tutorials , oracle database , c++ tutorials , java programming ") ;
 
